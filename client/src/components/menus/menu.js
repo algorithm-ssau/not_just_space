e7bf7b3fdfd13d86f795b2598467ac7010dc6cae
sssilnar@bk.ru <zinzinati8>
2020-04-29 16:36:11 +0400

    Страница "О форуме"

--- client/src/components/menus/menu.js
+++ client/src/components/menus/menu.js
@@ -1,21 +1,19 @@
 import React from 'react'
 import './menu_style.css'
-
-
+import {NavLink} from "react-router-dom";
 
 export const Menus = () => {
     return(
         <header class="header">
             <nav>
                 <ul_header className="nav_links">
-                    <li_header><a_header href="client/src/pages/About_forum.js">О форуме</a_header></li_header>
+                    <li_header><NavLink to="/client/src/pages/About_forum.js">О форуме</NavLink></li_header>
                     <li_header><a_header href="#">Программа</a_header></li_header>
                     <li_header><a_header href="#">Мероприятия</a_header></li_header>
                     <li_header><a_header href="#">Контакты</a_header></li_header>
-
                 </ul_header>
             </nav>
         </header>
-)
+        )
 }
 
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { MainPage } from './pages/MainPage'
+import {AboutForum} from "./pages/About_forum";
 
 export const useRoutes = () => {
     return (
@@ -8,6 +9,11 @@ export const useRoutes = () => {
             <Route path='/' exact>
                 <MainPage />
             </Route>
+
+            <Route path='/client/src/pages/About_forum.js' exact>
+                <AboutForum />
+            </Route>
+
             <Redirect to="/" />
         </Switch>
     )
