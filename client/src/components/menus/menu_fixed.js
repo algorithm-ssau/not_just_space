b974d75404f227979caed491af178960829805e9
sssilnar@bk.ru <zinzinati8>
2020-05-25 13:53:02 +0400

    Хедеры для форума и программы

--- client/src/components/menus/menu_fixed.js
+++ client/src/components/menus/menu_fixed.js
@@ -13,28 +13,28 @@ b.insertAdjacentHTML('beforeend', '<a class="back_to_top" title="Наверх"> 
 
 (function() {
     'use strict';
-function trackScroll() {
-    var scrolled = window.pageYOffset;
-    var coords = document.documentElement.clientHeight;
+    function trackScroll() {
+        var scrolled = window.pageYOffset;
+        var coords = document.documentElement.clientHeight;
 
-    if (scrolled > coords) {
-        goTopBtn.classList.add('back_to_top-show');
+        if (scrolled > coords) {
+            goTopBtn.classList.add('back_to_top-show');
+        }
+        if (scrolled < coords) {
+            goTopBtn.classList.remove('back_to_top-show');
+        }
     }
-    if (scrolled < coords) {
-        goTopBtn.classList.remove('back_to_top-show');
+
+    function backToTop() {
+        if (window.pageYOffset > 0) {
+            window.scrollBy(0, -30);
+            
+            setTimeout(backToTop, 0);
+        }
     }
-}
 
-function backToTop() {
-    if (window.pageYOffset > 0) {
-        window.scrollBy(0, -30);
-        
-        setTimeout(backToTop, 0);
-    }
-}
+    var goTopBtn = document.querySelector('.back_to_top');
 
-var goTopBtn = document.querySelector('.back_to_top');
-
-window.addEventListener('scroll', trackScroll);
-goTopBtn.addEventListener('click', backToTop);
+    window.addEventListener('scroll', trackScroll);
+    goTopBtn.addEventListener('click', backToTop);
 })();
