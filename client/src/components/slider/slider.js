c960625cc28df0925ed680d9a26c20b89f4fac65
ruzaliya135 <ruzalia.badrieva@yandex.ru>
2020-06-07 09:34:47 +0400

    Последние доработки главной страницы

--- client/src/components/slider/slider.js
+++ client/src/components/slider/slider.js
@@ -1,39 +1,71 @@
 import React, { useState } from 'react'
 import './slider.css'
+import goNextImg from './goNext.png'
+import goPrevImg from './goPrev.png'
 
-export const Slider = (props) => {
-    let [currentSlide, setCurrentSlide] = useState(0)
-    const goNext = () => {
-        setCurrentSlide(currentSlide === props.children.length - 1 ?
-            currentSlide = 0 :
-            currentSlide += 1);
-        console.log('next', props.children);
-    };
-    const goPrev = () => {
-        setCurrentSlide(currentSlide === 0 ?
-            currentSlide = props.children.length - 1 :
-            currentSlide -= 1);
-        console.log('prev', currentSlide);
-    };
-    return (
-        <div className="carousel-container">
-            <button
-                {...{
-                    onClick: goPrev
-                }}
-            >{'<'}</button>
 
-            <div className="slides-container">
-                {props.children[currentSlide === 0 ? props.children.length - 1 : currentSlide - 1]}
-                {props.children[currentSlide]}
-                {props.children[currentSlide === props.children.length - 1 ? 0 : currentSlide + 1]}
+export class Slider extends React.Component {
+    constructor(props) {
+        super(props)
+        this.currentSlide = 0;
+        this.slidesContainer = React.createRef();
+        this.disabledBtns = false;
+    }
+
+    render(props) {
+        var me = this;
+        const goNext = () => {
+            me.slidesContainer.current.classList.add('go-next-click');
+            me.setState({disabledBtns: true});
+            setTimeout(() => {
+                me.setState({currentSlide: me.currentSlide === me.props.children.length - 1 ?
+                    me.currentSlide = 0 :
+                    me.currentSlide += 1,
+                    disabledBtns: false
+                });
+                me.slidesContainer.current.classList.remove('go-next-click');
+            }, 990);
+        };
+        const goPrev = () => {
+            me.slidesContainer.current.classList.add('go-prev-click');
+            me.setState({disabledBtns: true});
+            setTimeout(() => {
+                me.setState({
+                    currentSlide: me.currentSlide === 0 ?
+                        me.currentSlide = me.props.children.length - 1 :
+                        me.currentSlide -= 1,
+                    disabledBtns: false
+                });
+                me.slidesContainer.current.classList.remove('go-prev-click');
+            }, 990);
+        };
+
+        return (
+            <div className="carousel-container">
+                <button
+                    {...{
+                        onClick: goPrev,
+                        disabled: me.disabledBtns
+                    }}
+                >
+                    <img src={goPrevImg} width="56" alt="<"/>
+                </button>
+
+                <div className="slides-container" ref={this.slidesContainer}>
+                    {me.props.children[me.currentSlide === 0 ? me.props.children.length - 1 : me.currentSlide - 1]}
+                    {me.props.children[me.currentSlide]}
+                    {me.props.children[me.currentSlide === me.props.children.length - 1 ? 0 : me.currentSlide + 1]}
+                </div>
+
+                <button
+                    {...{
+                        onClick: goNext,
+                        disabled: me.disabledBtns
+                    }}
+                >
+                    <img src={goNextImg} width="56" alt=">"/>
+                </button>
             </div>
-
-            <button
-                {...{
-                    onClick: goNext
-                }}
-            >{'>'}</button>
-        </div>
-    )
+        )
+    }
 }
