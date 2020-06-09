9d22bcd46f7d125404af4bdb351c8ba4f339de79
ruzaliya135 <ruzalia.badrieva@yandex.ru>
2020-06-10 03:02:51 +0400

    production настройки 5

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ app.use('/', express.static(path.join(__dirname, 'client/build')));
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
-const PORT = config.get('PORT') || 5000;
+const PORT = process.env.PORT || 5000;
 
 async function start() {
     try {
-        app.listen(5000, () => { console.log(`App has been started on PORT ${PORT}...`); });
+        app.listen(PORT, () => { console.log(`App has been started on PORT ${PORT}...`); });
     } catch (e) {
         console.log('Server Error', e.message, e);
         process.exit(1);
