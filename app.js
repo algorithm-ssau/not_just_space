acb1ca3b631fb0228d6a7c4ed00bfefd9827c93d
ruzaliya135 <ruzalia.badrieva@yandex.ru>
2020-06-10 02:32:15 +0400

    production настройки 4

--- app.js
+++ app.js
@@ -4,15 +4,12 @@ const config = require('config');
 
 const app = express();
 
+// Serve static files from the React app
+app.use('/', express.static(path.join(__dirname, 'client/build')));
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve static files from the React app
-    app.use('/', express.static(path.join(__dirname, 'client/build')));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 const PORT = config.get('PORT') || 5000;
 
 async function start() {
