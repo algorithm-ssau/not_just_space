d3f989c881cd3e29d5f0b7b937ced2819e1ac3a9
ruzaliya135 <ruzalia.badrieva@yandex.ru>
2020-06-10 01:38:10 +0400

    Настройки для production

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
+const path = require('path')
 const config = require('config');
 
 const app = express();
 
 const PORT = config.get('PORT') || 5000;
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve static files from the React app
+    app.use('/', express.static(path.join(__dirname, 'client/build')));
 
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 async function start() {
     try {
-        // await mongoose.connect(config.get('mongoUri'), {
-        //     useNewUrlParser: true,
-        //     useUnifiedTopology: true,
-        //     useCreateIndex: true
-        // });
         app.listen(5000, () => { console.log(`App has been started on PORT ${PORT}...`); });
     } catch (e) {
         console.log('Server Error', e.message, e);
